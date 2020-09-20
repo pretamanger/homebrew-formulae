5d4185f5e85e8b227196c51c449256e31fc15473
Giordano Scalzo <giordano.scalzo@gmail.com>
2020-09-20 17:16:17 +0100

    Update swift-format.rb

--- Formula/swift-format.rb
+++ Formula/swift-format.rb
@@ -1,10 +1,10 @@
 class SwiftFormat < Formula
     desc "`swift-format` provides the formatting technology for SourceKit-LSP and the building blocks for doing code formatting transformations."
     homepage "https://github.com/apple/swift-format"
-    url "https://github.com/apple/swift-format.git", :branch => "swift-5.2-branch"
-    version "0.0.2"
+    url "https://github.com/apple/swift-format.git", :branch => "swift-5.3-branch"
+    version "0.0.3"
 
-    depends_on :xcode => ["11.0", :build]
+    depends_on :xcode => ["12.0", :build]
 
     def install
         system "swift build -c release --disable-sandbox --build-path '.build'"
